e002fbd9c31f3cb17a00730b847cfa2a748e2af1
José Manuel Aguirre <jmaguirrei@gmail.com>
2019-01-30 10:47:58 -0300

    Initial commit

--- src/index.js
+++ src/index.js
@@ -1,7 +1,6 @@
-
-
 module.exports = function ({ types }) {
 
+  let sequential = 1000;
   let startAt = 1000;
   const visitor = {
 
@@ -9,12 +8,26 @@ module.exports = function ({ types }) {
 
       path.node.properties.forEach(property => {
         if (property.key && property.key.name === 'render') {
+
           const uid = path.scope.generateUidIdentifier('').name;
           const id = Number(uid.substr(1)) + startAt;
           path.node.properties.push(
             types.ObjectProperty(
               types.Identifier('id'),
               types.NumericLiteral(id),
+            )
+          );
+          path.node.properties.push(
+            types.ObjectProperty(
+              types.Identifier('scope'),
+              types.NumericLiteral(path.context.scope.uid),
+            )
+          );
+
+          path.node.properties.push(
+            types.ObjectProperty(
+              types.Identifier('sequential'),
+              types.NumericLiteral(sequential++),
             )
           );
         }
@@ -29,29 +42,4 @@ module.exports = function ({ types }) {
 
 };
 
-// module.exports = function (babel) {
 
-//   let sequential = 1000;
-//   const { ObjectProperty, Identifier, NumericLiteral } = babel.types;
-
-//   const visitor = {
-
-//     ObjectExpression({ node }) {
-
-//       node.properties.forEach(property => {
-//         if (property.key && property.key.name === 'render') {
-//           node.properties.push(
-//             ObjectProperty(Identifier('id'), NumericLiteral(sequential))
-//           );
-//           sequential++;
-//         }
-//       });
-//     }
-
-//   };
-
-//   return {
-//     visitor,
-//   };
-
-// };
